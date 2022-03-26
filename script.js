f8ffab0a244d2d9374f4678f1230e563ae3e98de
Kimberly West <kdenton993@gmail.com>
2022-03-26 10:01:22 -0600

    declared function with fetch to api

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // declar variables and grabbing elements from html
-var cityEl= document.getElementById("enter-city");
+var cityEl = document.getElementById("enter-city");
 var searchEl = document.getElementById("search-button");
 var clearEl = document.getElementById("clear-history");
 var nameEl = document.getElementById("city-name");
@@ -15,12 +15,25 @@ var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 //unique API Key
 var APIKey = "8582006685026ce77e41f2399696a305";
 
-
-function page(){
-
-function getWeather(cityName) {
+function page() {
+  function getWeather(cityName) {
     //get weather request from open weather
-    let queryURL = "https://api.openweathermap.org/data.2.5/weatherq=" + cityName + "&appid=" + APIKey;
-    axios.get
-}
+    let queryURL =
+      "https://api.openweathermap.org/data.2.5/weatherq=" +
+      cityName +
+      "&appid=" +
+      APIKey;
+    axios.get(queryURL).then(function (response) {
+      todayEl.classList.remove("d-none");
+      //parse response to current weather
+      var currentDate = new Date(response.data.dt * 1000);
+      var day = currentDate.getDate();
+      var month = currentDate.getMonth() + 1;
+      var year = currentDate.getFullYear();
+      nameEl.innerHTML =
+        response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+        var weatherPic = response.data.weather[0].icon;
+        currentPicEl.setAttribute
+    });
+  }
 }
